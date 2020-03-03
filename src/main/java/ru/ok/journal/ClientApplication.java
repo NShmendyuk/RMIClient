6d03658bbbd68274d9758d9fb7e6df513a09e2cd
NEE_TT <Nshmendyuk.ru@gmail.com>
2020-03-03 16:12:27 +0300

    added entity (path to entity in client and server have to be the same)

--- src/main/java/ru/ok/journal/ClientApplication.java
+++ src/main/java/ru/ok/journal/ClientApplication.java
@@ -3,6 +3,7 @@ package ru.ok.journal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import ru.ok.journal.remote.entity.RMIComment;
 import ru.ok.journal.remote.rmi.IRMIComment;
 import ru.ok.journal.remote.rmi.Pair;
 
@@ -16,13 +17,15 @@ public class ClientApplication {
 //        SpringApplication.run(ClientApplication.class, args);
 //        IRMIComment getRMIComments = SpringApplication.run(ClientApplication.class, args)
 //                .getBean(IRMIComment.class);
+
         ApplicationContext app = SpringApplication.run(ClientApplication.class, args);
         IRMIComment getRMIComments = app.getBean(IRMIComment.class);
 
         System.out.println("================Client Side ========================");
-        Pair pair = new Pair(2,0);
+        Pair pair = new Pair(1,1);
         List<Pair> postIds = new ArrayList<>();
         postIds.add(pair);
-        System.out.println(getRMIComments.getByPost(postIds));
+        List<RMIComment> comments = getRMIComments.getByPost(postIds);
+        System.out.println(comments);
     }
 }
